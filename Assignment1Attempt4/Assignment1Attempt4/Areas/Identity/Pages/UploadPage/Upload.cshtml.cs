6f59e3837997e1fa10b2260eb61960674369a1a0
Zack Walker <zackwalker@mail.weber.edu>
2023-09-17 10:48:13 -0600

    Put the base down for the Upload Page

---
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/UploadPage/Upload.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public class UploadModel : PageModel
+{
+    [BindProperty]
+    public IFormFile Picture { get; set; }
+
+    public string PicturePath { get; private set; }
+
+    public async Task OnPostAsync()
+    {
+        if (Picture != null && Picture.Length > 0)
+        {
+
+            var uploadsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+
+            if (!Directory.Exists(uploadsDirectory))
+            {
+                Directory.CreateDirectory(uploadsDirectory);
+            }
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(Picture.FileName);
+            var filePath = Path.Combine(uploadsDirectory, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await Picture.CopyToAsync(stream);
+            }
+
+            PicturePath = $"/uploads/{fileName}";
+        }
+    }
+}
