683ae31062f7bfcc0941c5c2a5ffc74f4e7c53cc
Axis60 <112454625+Axis60@users.noreply.github.com>
2023-09-20 12:55:58 -0600

    UserEditPage
    Added a page with a form that has fields for the user to enter info, and meant for alter db data for the account.
    
    There is not yet a func to connect to the local db, for pulling user data or writing back to the db.

---
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Data/Assignment1Attempt4Context.cs
@@ -0,0 +1,22 @@
+﻿using Assignment1Attempt4.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment1Attempt4.Data;
+
+public class Assignment1Attempt4Context : IdentityDbContext<Assignment1Attempt4User>
+{
+    public Assignment1Attempt4Context(DbContextOptions<Assignment1Attempt4Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
