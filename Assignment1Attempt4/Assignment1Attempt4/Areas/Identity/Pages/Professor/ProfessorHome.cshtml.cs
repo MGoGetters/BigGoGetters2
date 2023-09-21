c109381649089f9861779b2d5cfcd4cfdc612689
xleon1 <144269577+xleon00wsu@users.noreply.github.com>
2023-09-21 12:55:29 -0600

    Added Student and Professor folders and "Welcome" pages to the project in the correct spot.

---
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/Professor/ProfessorHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment1Attempt4.Areas.Identity.Pages.Professor
+{
+    public class ProfessorHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/Student/StudentHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment1Attempt4.Areas.Identity.Pages.Student
+{
+    public class StudentHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
