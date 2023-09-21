26ec349ee3f3468413e2e37a6a1a675b2cb20af2
Zack Walker <zackwalker@mail.weber.edu>
2023-09-21 15:21:44 -0600

    Created a Test Profile Page

---
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/ProfileTestPage/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment1Attempt4.Areas.Identity.Pages.ProfileTestPage
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
