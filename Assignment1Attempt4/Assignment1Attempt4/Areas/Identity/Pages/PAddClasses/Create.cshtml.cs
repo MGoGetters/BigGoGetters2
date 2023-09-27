dc60800bb428c90216cb5b1bd2dcf01b8dc512d0
Mylee Anderson <myleeanderson@mail.weber.edu>
2023-09-27 13:36:55 -0600

    Added validation for time

--- Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/PAddClasses/Create.cshtml.cs
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/PAddClasses/Create.cshtml.cs
@@ -31,6 +31,22 @@ namespace Assignment1Attempt4.Areas.Identity.Pages.PAddClasses
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
+            int testDate = DateTime.Compare(Classes.StartTime, Classes.EndTime);
+
+            //if start time is later than end time it returns a 1
+            if (testDate > 0)
+            {
+                ModelState.AddModelError("Classes.StartTime", "Start Time cannot be later than End Time");
+            }
+
+            testDate = DateTime.Compare(Classes.EndTime, Classes.StartTime);
+
+            //if end time is earlier than start time it returns a -1
+            if (testDate < 0)
+            {
+                ModelState.AddModelError("Classes.EndTime", "End Time cannot be earlier than Start Time");
+            }
+
           if (!ModelState.IsValid || _context.Classes == null || Classes == null)
             {
                 return Page();
--- Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/PAddClasses/Edit.cshtml.cs
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/PAddClasses/Edit.cshtml.cs
@@ -43,6 +43,22 @@ namespace Assignment1Attempt4.Areas.Identity.Pages.PAddClasses
         // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
+            int testDate = DateTime.Compare(Classes.StartTime, Classes.EndTime);
+
+            //if start time is later than end time it returns a 1
+            if (testDate > 0)
+            {
+                ModelState.AddModelError("Classes.StartTime", "Start Time cannot be later than End Time");
+            }
+
+            testDate = DateTime.Compare(Classes.EndTime, Classes.StartTime);
+
+            //if end time is earlier than start time it returns a -1
+            if (testDate < 0)
+            {
+                ModelState.AddModelError("Classes.EndTime", "End Time cannot be earlier than Start Time");
+            }
+
             if (!ModelState.IsValid)
             {
                 return Page();
