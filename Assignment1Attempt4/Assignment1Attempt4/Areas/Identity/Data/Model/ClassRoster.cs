1902da843de661ccc514a14c32a29ad295e6d501
xleon1 <144269577+xleon00wsu@users.noreply.github.com>
2023-10-04 23:26:00 -0600

    Finishing touches on RegisterForClass & Added ClassRoster model

---
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Data/Model/ClassRoster.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1Attempt4.Areas.Identity.Data.Model
+{
+    public class ClassRoster
+    {
+        // Student ID
+        public int ID { get; set; }
+        public string SFName { get; set; }
+        public string SLName { get; set; }
+
+    }
+}
