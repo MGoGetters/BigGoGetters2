1f140b58b472b435e16a8fd726a1dc70cf7a8452
Zack Walker <zackwalker@mail.weber.edu>
2023-10-23 13:56:45 -0600

    Fixed Extension Submissions

--- Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/AssignmentUpload/AssignmentUpload.cshtml.cs
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/AssignmentUpload/AssignmentUpload.cshtml.cs
@@ -17,7 +17,6 @@ namespace Assignment1Attempt4.Areas.Identity.Pages.AssignmentUpload
         public string TextInput { get; set; }
 
         public string TextFilePath { get; private set; }
-
         public async Task OnPostAsync()
         {
             var assignmentName = Request.Form["assignmentName"];
@@ -28,25 +27,40 @@ namespace Assignment1Attempt4.Areas.Identity.Pages.AssignmentUpload
             {
                 Directory.CreateDirectory(uploadsDirectory);
             }
+
             var firstName = HttpContext.Session.GetString("CurrentFName");
             var lastName = HttpContext.Session.GetString("CurrentLName");
 
-            var fileName = $"{firstName}_{lastName}_{DateTime.Now:yyyyMMddHHmmss}.txt"; 
-            var filePath = Path.Combine(uploadsDirectory, fileName);
+            string fileName;
 
-            if (!string.IsNullOrEmpty(TextInput))
+            if (TextFile != null && TextFile.Length > 0)
             {
-                await System.IO.File.WriteAllTextAsync(filePath, TextInput);
-            }
-            else if (TextFile != null && TextFile.Length > 0)
-            {
+                string originalExtension = Path.GetExtension(TextFile.FileName);
+                if (originalExtension.Equals(".txt", StringComparison.OrdinalIgnoreCase))
+                {
+                    fileName = $"{firstName}_{lastName}_{DateTime.Now:yyyyMMddHHmmss}.txt";
+                }
+                else
+                {
+                    fileName = $"{firstName}_{lastName}_{DateTime.Now:yyyyMMddHHmmss}{originalExtension}";
+                }
+
+                var filePath = Path.Combine(uploadsDirectory, fileName);
+
                 using (var stream = new FileStream(filePath, FileMode.Create))
                 {
                     await TextFile.CopyToAsync(stream);
                 }
-            }
 
-            TextFilePath = $"/submissions/{assignmentName}/{fileName}";
+                TextFilePath = $"/submissions/{assignmentName}/{fileName}";
+            }
+            else if (!string.IsNullOrEmpty(TextInput))
+            {
+                fileName = $"{firstName}_{lastName}_{DateTime.Now:yyyyMMddHHmmss}.txt";
+                var filePath = Path.Combine(uploadsDirectory, fileName);
+                await System.IO.File.WriteAllTextAsync(filePath, TextInput);
+                TextFilePath = $"/submissions/{assignmentName}/{fileName}";
+            }
         }
     }
 }
