0d0e33dfa56e9618a9b52a57129011431ef25cbd
Robert Michael Milliner <robertmilliner@mail.weber.edu>
2023-11-03 15:14:42 -0600

    Deleted unused files. Added nav to some pages

--- Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/ProfileTestPage/Profile.cshtml.cs
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/ProfileTestPage/Profile.cshtml.cs
@@ -1,3 +1,7 @@
+using Assignment1Attempt4.Areas.Identity.Data;
+using Assignment1Attempt4.Controllers;
+using Assignment1Attempt4.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,35 @@ namespace Assignment1Attempt4.Areas.Identity.Pages.ProfileTestPage
 {
     public class ProfileModel : PageModel
     {
+
+        private readonly CheckoutApiController _checkoutController;
+        private readonly Assignment1Attempt4.Data.Assignment1Attempt4DBContext _context;
+        private readonly ILogger<CheckoutApiController> _logger;
+        private readonly UserManager<Assignment1Attempt4User> _userManager;
+
+        public ProfileModel(CheckoutApiController checkoutController, Assignment1Attempt4DBContext context, ILogger<CheckoutApiController> logger, UserManager<Assignment1Attempt4User> userManager)
+        {
+            _checkoutController = checkoutController;
+            _context = context;
+            _logger = logger;
+            _userManager = userManager;
+        }
+
         public void OnGet()
         {
+
+            var userid = _userManager.GetUserId(this.User);
+            var myUser = _userManager.Users.Where(x => x.Id == userid).ToList()[0];
+
+            HttpContext.Session.SetString("CurrentFName", myUser.FirstName);
+            HttpContext.Session.SetString("CurrentLName", myUser.LastName);
+            HttpContext.Session.SetString("StuOrProf", myUser.profOrStudent);
+            HttpContext.Session.SetInt32("UserID", myUser.UserNumber);
+
+            ViewData["UserID"] = myUser.UserName;
+            ViewData["FirstName"] = HttpContext.Session.GetString("CurrentFName");
+            ViewData["LastName"] = HttpContext.Session.GetString("CurrentLName");
+            ViewData["StuOrProf"] = HttpContext.Session.GetString("StuOrProf");
         }
     }
 }
