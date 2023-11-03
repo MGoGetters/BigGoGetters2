1e75055d69fd2bafea32eda804731d0515c47ef6
Robert Michael Milliner <robertmilliner@mail.weber.edu>
2023-11-03 16:42:11 -0600

    Locked Checkout/Cancel & Checkout/Success to student view

--- Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/Checkout/Cancel.cshtml.cs
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/Checkout/Cancel.cshtml.cs
@@ -1,3 +1,7 @@
+using Assignment1Attempt4.Areas.Identity.Data;
+using Assignment1Attempt4.Controllers;
+using Assignment1Attempt4.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +9,23 @@ namespace Assignment1Attempt4.Areas.Identity.Pages.Checkout
 {
     public class CancelModel : PageModel
     {
+        private readonly Assignment1Attempt4.Data.Assignment1Attempt4DBContext _context;
+        private readonly UserManager<Assignment1Attempt4User> _userManager;
+
+
+
+        public CancelModel(UserManager<Assignment1Attempt4User> userManager, Assignment1Attempt4DBContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public string stuOrProf { get; set; }
+
         public void OnGet()
         {
+            stuOrProf = HttpContext.Session.GetString("StuOrProf");
         }
+
     }
 }
--- Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/Checkout/Success.cshtml.cs
+++ Assignment1Attempt4/Assignment1Attempt4/Areas/Identity/Pages/Checkout/Success.cshtml.cs
@@ -1,3 +1,6 @@
+using Assignment1Attempt4.Areas.Identity.Data;
+using Assignment1Attempt4.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,23 @@ namespace Assignment1Attempt4.Areas.Identity.Pages.Checkout
 {
     public class SuccessModel : PageModel
     {
+        private readonly Assignment1Attempt4.Data.Assignment1Attempt4DBContext _context;
+        private readonly UserManager<Assignment1Attempt4User> _userManager;
+
+
+
+        public SuccessModel(UserManager<Assignment1Attempt4User> userManager, Assignment1Attempt4DBContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public string stuOrProf { get; set; }
+
         public void OnGet()
         {
+            stuOrProf = HttpContext.Session.GetString("StuOrProf");
         }
+
     }
 }
