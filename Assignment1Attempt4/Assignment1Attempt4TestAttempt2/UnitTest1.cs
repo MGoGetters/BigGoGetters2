4818b70f4b845dce6b6fa3a94e8aaf39fe26bde9
Robert Michael Milliner <robertmilliner@mail.weber.edu>
2023-11-08 11:07:20 -0700

    added 3rd unit test

--- Assignment1Attempt4/Assignment1Attempt4TestAttempt2/UnitTest1.cs
+++ Assignment1Attempt4/Assignment1Attempt4TestAttempt2/UnitTest1.cs
@@ -3,6 +3,7 @@ using Assignment1Attempt4.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Linq;
+using System;
 using System.Data;
 using System.Drawing.Text;
 using System.Security;
@@ -87,11 +88,83 @@ namespace Assignment1Attempt4TestAttempt2
             Assert.AreNotEqual(initialStudentInCourseCount, postStudentInCourseCount);
         }
 
+        [TestMethod]
+        public void InstructorCanCreateAssignemntTest()
+        {
 
-        //public void InstructorCanCreateAssignemntTest()
-        //{
+            ////First version of the test attempts to check the assignments that are in the classes only taught by a given professor
+            ////it does not work pretty sure issue is how im trying to pull the data
 
-        //}
+            //int profID = 1;
+            //int initialAssignmentCount = 0;
+
+            ////count initial assignments
+            //IQueryable<Classes> ProfClasses = Context.Classes.Where(c => c.ProfessorID == profID);
+            //for (int i = 0; i < ProfClasses.Count(); i++)
+            //{
+            //    var tempClass = ProfClasses.ElementAt(i).ProfessorID;
+            //    initialAssignmentCount = initialAssignmentCount + Context.Assignments.Where(a => a.ClassesID == tempClass).Count();
+            //}
+
+            ////add an assignments here
+            //Assignments newAssignment = new Assignments
+            //{
+            //    AssignmentName = "TEST " + initialAssignmentCount,
+            //    Description = "Test " + initialAssignmentCount,
+            //    MaxPoints = 100,
+            //    DueDate = DateTime.Now.AddDays(14),
+            //    AssignmentType = "Text",
+            //    ClassesID = 1, //can make ClassID = to a known real class if i remake full db everytime lmao
+            //};
+
+
+            //int testinit = Context.Assignments.Count();
+
+            ////save changes to db
+            //Context.Assignments.Add(newAssignment);
+            //Context.SaveChanges();
+
+            //int testpost = Context.Assignments.Count();
+
+
+            ////count post assignments
+            //int postAssignmentCount = 0;
+            //ProfClasses = Context.Classes.Where(c => c.ProfessorID == profID);
+            //for (int i = 0; i < ProfClasses.Count(); i++)
+            //{
+            //    var tempClass = ProfClasses.ElementAt(i).ProfessorID;
+            //    postAssignmentCount = postAssignmentCount + Context.Assignments.Where(a => a.ClassesID == tempClass).Count();
+            //}
+
+            ////check if assignment was added
+            //Assert.AreNotEqual(initialAssignmentCount, postAssignmentCount);
+
+            //---------------------------------------------------------------------------------------------------------------------
+
+
+
+            ////second version of this test just checks the total number of assignments by any prof
+            ////it 100% works but not sure what we want to do 
+            int initialAssignmentCount = 0;
+
+            Assignments newAssignment = new Assignments
+            {
+                AssignmentName = "TEST " + initialAssignmentCount,
+                Description = "Test " + initialAssignmentCount,
+                MaxPoints = 100,
+                DueDate = DateTime.Now.AddDays(14),
+                AssignmentType = "Text",
+                ClassesID = 1, //can make ClassID = to a known real class if i remake full db everytime lmao
+            };
+
+            int testinit = Context.Assignments.Count();
+
+            Context.Assignments.Add(newAssignment);
+            Context.SaveChanges();
+
+            int testpost = Context.Assignments.Count();
+            Assert.AreNotEqual(testinit, testpost);
+        }
 
         //public void StudentCanSubmitTextEntryTest()
         //{
